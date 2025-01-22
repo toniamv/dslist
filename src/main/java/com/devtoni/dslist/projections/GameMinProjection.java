3dd000672bf1a78afc7f0810491abf8f8e40d36a
Gabriel Antonio <toni.gamv@gmail.com>
2025-01-22 10:38:53 -0300

    Homologação Realizada - postgres do perfil dev funcionando

--- src/main/java/com/devtoni/dslist/projections/GameMinProjection.java
+++ src/main/java/com/devtoni/dslist/projections/GameMinProjection.java
@@ -3,7 +3,7 @@ package com.devtoni.dslist.projections;
 public interface GameMinProjection {
     Long getId();
     String getTitle();
-    Integer getYear();
+    Integer getGameYear();
     String getImgUrl();
     String getShortDescription();
     Integer getPosition();
