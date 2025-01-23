34fdc061de2578c4da8f2c4ea74edd6e62a10e51
Gabriel Antonio <toni.gamv@gmail.com>
2025-01-23 10:10:24 -0300

    Endpoint de movimentação de itens na lista adicionado

--- src/main/java/com/devtoni/dslist/controllers/GameListController.java
+++ src/main/java/com/devtoni/dslist/controllers/GameListController.java
@@ -2,13 +2,11 @@ package com.devtoni.dslist.controllers;
 
 import com.devtoni.dslist.dto.GameListDTO;
 import com.devtoni.dslist.dto.GameMinDTO;
+import com.devtoni.dslist.dto.ReplacementDTO;
 import com.devtoni.dslist.services.GameListService;
 import com.devtoni.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,5 +30,10 @@ public class GameListController {
     public List<GameMinDTO> findByList(@PathVariable Long listId){
         List<GameMinDTO> result = gameService.findbyList(listId);
         return result;
+    }
+
+    @PostMapping(value = "/{listId}/replacement")
+    public void move(@PathVariable Long listId, @RequestBody ReplacementDTO body){
+        gameListService.move(listId, body.getSourceIndex(), body.getDestinationIndex());
     }
 }
